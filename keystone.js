86403ce509a468010304039bf4e2298954c996eb
Andreea Dumitrascu <andreea.dumitrascu57@gmail.com>
2022-09-28 17:24:16 +0300

    [master] Configure local storage

--- keystone.js
+++ keystone.js
@@ -1,8 +1,21 @@
 import { config } from '@keystone-6/core';
 import { lists } from './lists.js'
 
+import dotenv from 'dotenv'
+
 import { insertSeedData } from "./seed-data";
 import { session, withAuth } from "./auth";
+
+dotenv.config();
+
+const {
+  S3_BUCKET_NAME: bucketName = 'keystone-test',
+  S3_REGION: region = 'ap-southeast-2',
+  S3_ACCESS_KEY_ID: accessKeyId = 'keystone',
+  S3_SECRET_ACCESS_KEY: secretAccessKey = 'keystone',
+  ASSET_BASE_URL: baseUrl = 'http://localhost:5003',
+ } = process.env;
+
 
 export default withAuth(
   config({
@@ -13,8 +26,19 @@ export default withAuth(
           await insertSeedData(context);
       }
     },
+    storage: {
+      my_local_images: {
+        kind: 'local',
+        type: 'image',
+        generateUrl: path => `${baseUrl}/images${path}`,
+        serverRoute: {
+          path: '/images',
+        },
+        storagePath: 'public/images',
+      }
+    },
     server: {
-      port: 5002,
+      port: 5003,
       cors: true
     },
     ui: {
